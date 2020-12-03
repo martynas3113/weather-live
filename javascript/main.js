6a3cc376ce61a0e2ae7036f4ccba986c6b4af8a6
martynas3113 <martynas3113@users.noreply.github.com>
2020-12-04 00:13:45 +0200

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -12,7 +12,7 @@ window.addEventListener('load', () => {
             lat = position.coords.latitude;
 
 
-            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&lang=lt&units=metric&appid=e8fc1f96fd784a1d4401b66853e10254`
+            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=e8fc1f96fd784a1d4401b66853e10254`
             fetch(api)
                 .then(response => {
                     return response.json()
