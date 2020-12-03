b308ea9c1c62a4e4b40c95c75fbf9417e1f4bd95
martynas3113 <martynas3113@users.noreply.github.com>
2020-12-03 23:48:47 +0200

    live-weather

---
+++ javascript/main.js
@@ -0,0 +1,35 @@
+window.addEventListener('load', () => {
+    let long;
+    let lat;
+    let temperatureDescriptionDOM = document.querySelector('.temperature-description')
+    let temperatureDegreeDOM = document.querySelector('.degree-container > .degree')
+    let timezoneDOM = document.querySelector('.location >.timezone')
+    let iconDOM = document.querySelector('.location > p')
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+
+            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=e8fc1f96fd784a1d4401b66853e10254`
+            fetch(api)
+                .then(response => {
+                    return response.json()
+                })
+                .then(data => {
+                    console.log(data);
+                    const temperature = data.main.temp;
+                    const timezone = data.name;
+                    const description = data.weather[0].description;
+                    temperatureDegreeDOM.textContent = temperature;
+                    timezoneDOM.textContent = timezone;
+                    temperatureDescriptionDOM.textContent = description
+
+                });
+        });
+
+    }
+
+   
+});
